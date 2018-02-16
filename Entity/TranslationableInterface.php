3986bfeb8bcf56a1c3a65a35928ad24f18e18b5b
Alexander Schranz <alexander.schranz@massiveart.com>
2018-02-16 08:48:18 +0100

    add translationinterfaces and traits

---
+++ Entity/TranslationableInterface.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * This file is part of ChekovEventBundle package.
+ *
+ * (c) Chekov Bundles <https://github.com/pavel-chekov>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Chekov\Bundle\ExtensionBundle\Entity;
+
+use Doctrine\Common\Collections\Collection;
+
+/**
+ * Interface TranslationableInterface
+ *
+ * @package Chekov\Bundle\ExtensionBundle\Entity
+ */
+interface TranslationableInterface
+{
+    /**
+     * Add translation
+     *
+     * @param TranslationInterface $translation
+     *
+     * @return $this
+     */
+    public function addTranslation(TranslationInterface $translation);
+
+    /**
+     * Remove translation
+     *
+     * @param TranslationInterface $translation
+     *
+     * @return $this
+     */
+    public function removeTranslation(TranslationInterface $translation);
+
+    /**
+     * Get translations
+     *
+     * @return Collection|TranslationInterface[]
+     */
+    public function getTranslations();
+
+    /**
+     * @param string $locale
+     *
+     * @return TranslationInterface|null
+     */
+    public function getTranslation($locale);
+}
